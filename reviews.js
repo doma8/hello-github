4b9289a15ff9d6b2af14a85857ea166568263006
doma8 <132088995+doma8@users.noreply.github.com>
2023-06-09 21:12:22 +0300

    Add files via upload

---
+++ reviews.js
@@ -0,0 +1,84 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'fashion designer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "i really like the collection",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: 'I bought alot of types all are good .',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: 'Iprefer the dark colors.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+  // select items
+  const img = document.getElementById('person-img');
+  const author = document.getElementById('author');
+  const job = document.getElementById('job');
+  const info = document.getElementById('info');
+  
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const randomBtn = document.querySelector('.random-btn');
+  
+  // set starting item
+  let currentItem = 0;
+  
+  // load initial item
+  window.addEventListener('DOMContentLoaded', function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  });
+  
+  // show person based on item
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  // show next person
+  nextBtn.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener('click', function () {
+    console.log('hello');
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
